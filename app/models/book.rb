7616d38930b528dbc7e1c6339271a2b2663fb1e1
Prabakar Battula <prabhakar.battula@gmail.com>
2014-04-24 06:40:43 +0530

    added validations to Book model and also test for the same

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,5 @@ class Book < ActiveRecord::Base
 	belongs_to :credit, class_name: "Ledger"
 
 	validates_presence_of :debit_id, :credit_id, :date, :amount
+	validates_numericality_of :amount, greater_than_or_equal_to: 1
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_presence_of_amount
+	  ledger1 = Ledger.create(name: 'petrol')
+	  ledger2 = Ledger.create(name: 'cash')
+	  ledger3 = Ledger.create(name: 'pooja')
+
+	  book1 = Book.create(date: 2014-04-23, debit_id: ledger1.id, credit_id: ledger2.id, amount: 0)
+
+		assert_not_equal true, book1.valid?
+  end
 end
