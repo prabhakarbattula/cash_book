265c30cc1364edcb7ad93490d13735d286324348
Prabakar Battula <prabhakar.battula@gmail.com>
2014-04-29 07:09:24 +0530

    modified tests using FIXTURES

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -2,12 +2,33 @@ require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
   def test_presence_of_amount
-	  ledger1 = Ledger.create(name: 'petrol')
-	  ledger2 = Ledger.create(name: 'cash')
-	  ledger3 = Ledger.create(name: 'pooja')
-
-	  book1 = Book.create(date: 2014-04-23, debit_id: ledger1.id, credit_id: ledger2.id, amount: 0)
+	  book1 = books(:one)
 
 		assert_not_equal true, book1.valid?
   end
+
+  def test_presence_of_date
+	  book1 = books(:two)
+
+	  assert_not_equal true, book1.valid?
+  end
+
+  def test_presence_of_debit_id
+	  book1 = books(:three)
+
+	  assert_not_equal true, book1.valid?
+  end
+
+  def test_presence_of_credit_id
+	  book1 = books(:four)
+
+	  assert_not_equal true, book1.valid?
+  end
+
+  def test_all_present
+	  book1 = books(:five)
+
+	  assert_equal true, book1.valid?
+  end
+
 end
--- test/models/ledger_test.rb
+++ test/models/ledger_test.rb
@@ -2,12 +2,11 @@ require 'test_helper'
 
 class LedgerTest < ActiveSupport::TestCase
   def test_checking_validations
-		Ledger.create(name: 'petrol')
-		ledger2 = Ledger.new(name: 'PETROL')
+		ledger1 = ledgers(:two)
+			ledger2 = Ledger.new(name: 'PETROL')
 
-		assert_equal false, ledger2.valid?
+		assert_not_equal true, ledger2.valid?
   end
-
 
 
   def test_check_trans_b4_del
@@ -16,12 +15,11 @@ class LedgerTest < ActiveSupport::TestCase
 	  ledger2 = Ledger.create(name: 'cash')
 
 
-	  Book.create(date: 2014-04-23, debit_id: ledger1, credit_id: ledger2)
+	  Book.create(date: 2014-04-23, debit_id: ledger1.id, credit_id: ledger2.id, amount: 1)
 
 	  if Book.find_by_debit_id(ledger1.id).present? || Book.find_by_credit_id(ledger1.id).present?
-
-	      else
-		      ledger1.destroy
+	  else
+		    ledger1.destroy
 		end
 
 	  assert_equal true, ledger1.present?
@@ -35,12 +33,12 @@ class LedgerTest < ActiveSupport::TestCase
 	  ledger2 = Ledger.create(name: 'cash')
 	  ledger3 = Ledger.create(name: 'pooja')
 
-	  book1 = Book.create(date: 2014-04-23, debit_id: ledger1, credit_id: ledger2)
+	  Book.create(date: 2014-04-23, debit_id: ledger1.id, credit_id: ledger2.id, amount: 1)
 
 	  if Book.find_by_debit_id(ledger3.id).present? || Book.find_by_credit_id(ledger3.present?)
-
-	  else
 		  ledger3.destroy
+	  else
+
 	  end
 
 	  assert_equal true, ledger3.destroyed?
