2126829e316122ccfe6394eba4c3e551db6f5028
PRABHAKAR BATTULA <prabhakar@bigbinary.com>
2016-03-19 20:20:53 +0530

    Assets pre compiled

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -117,6 +117,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:date, :debit_id, :credit_id, :amount)
+      params.require(:book).permit(:date, :debit_id, :credit_id, :amount, :remark)
     end
 end
